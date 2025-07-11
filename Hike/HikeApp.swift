f983ffd7b9416b778b803ac0fd063e1102532c21
Bhavin <bhavin.chauhan@lystloc.com>
2025-07-11 15:22:32 +0530

    Initial Commit

---
+++ Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by Bhavin Chauhan on 11/07/25.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
